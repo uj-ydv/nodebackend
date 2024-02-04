70bc5a615a5bf20a1c15ac7c59e4f19294f26d57
Ujjwal <ujjwal.yadav@servian.com>
2024-02-04 20:47:10 +1100

    Fixed api db functions in controller

--- controller/comment.js
+++ controller/comment.js
@@ -1,90 +1,74 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
+import Comment from '../models/comment.js';
 
-// const app = express.Router();
 
-// // Define a comment schema
-// const commentSchema = new mongoose.Schema({
-//   name: String,
-//   email: String,
-//   movie_id: String,
-//   text: String,
-//   date: Date,
-// });
+// Get all comments
+export const getAllComments = async (req, res) => {
+    try {
+        const comments = await Comment.find();
+        res.json(comments);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
 
-// const Comment = mongoose.model('Comment', commentSchema);
 
-// // Middleware to parse JSON
-// app.use(express.json());
+// Create a new comment
+export const createComment = async (req, res) => {
+    try {
+        const comment = new Comment(req.body);
+        const savedComment = await comment.save();
+        res.json(savedComment);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
 
-// // Routes
-// // define routes
 
-// // Get all comments
-// app.get('/', async (req, res) => {
-//   try {
-//     const comments = await Comment.find();
-//     res.json(comments);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
+// Get a comment by id
+export const getOneComment = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const comment = await Comment.findById(id);
+        res.json(comment);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
 
-// // Create a new comment
-// app.post('/', async (req, res) => {
-//   try {
-//     const comment = new Comment(req.body);
-//     const savedComment = await comment.save();
-//     res.json(savedComment);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
 
-// // Get a comment by id
-// app.get('/:id', async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const comment = await Comment.findById(id);
-//     res.json(comment);
-//   } catch (error) {
-//     res.status(500).json({ error: error.message });
-//   }
-// });
+// Update a comment by id
+export const updateComment = async (req, res) => {
+    try{
+        const { id } = req.params;
+        const comment = await Comment.findById(id);
+        const { name, email, movie_id, text, date } = req.body;
+        if (name) comment.name = name;
+        if (email) comment.email = email;
+        if (movie_id) comment.movie_id = movie_id;
+        if (text) comment.text = text;
+        if (date) comment.date = date;
+        await comment.save();
+        res.json(comment);
+    }
+    catch(error){
+        res.status(500).json({ error: error.message });
+    }
+};
 
-// // Update a comment by id
-// app.patch('/:id', async (req, res) => {
-//     try{
-//         const { id } = req.params;
-//         const comment = await Comment.findById(id);
-//         const { name, email, movie_id, text, date } = req.body;
-//         if (name) comment.name = name;
-//         if (email) comment.email = email;
-//         if (movie_id) comment.movie_id = movie_id;
-//         if (text) comment.text = text;
-//         if (date) comment.date = date;
-//         await comment.save();
-//         res.json(comment);
-//     }
-//     catch(error){
-//         res.status(500).json({ error: error.message });
-//     }
-// });
 
-// // Delete a comment by id
-// app.delete('/:id', async (req, res) => {
-//     try{
-//         const { id } = req.params;
-//         const comment = await Comment.findById(id);
-//         if (!comment) {
-//             return res.status(404).json({ error: 'Comment not found' });
-//         }
-//         const result = await Comment.deleteOne({ _id: id }); 
-//         res.json({ message: 'Comment deleted' });
-//     }
-//     catch(error){
-//         res.status(500).json({ error: error.message });
-//     }
-// });
+// Delete a comment by id
+export const deleteComment = async (req, res) => {
+    try{
+        const { id } = req.params;
+        const comment = await Comment.findById(id);
+        if (!comment) {
+            return res.status(404).json({ error: 'Comment not found' });
+        }
+        const result = await Comment.deleteOne({ _id: id }); 
+        res.json({ message: 'Comment deleted' });
+    }
+    catch(error){
+        res.status(500).json({ error: error.message });
+    }
+};
 
-// module.exports = app;
