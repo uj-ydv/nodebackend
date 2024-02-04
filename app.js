cb7acff148106ce47b99da008572e6eea5dd3db8
Ujjwal <ujjwal.yadav@servian.com>
2024-02-04 21:38:38 +1100

    Fixed uri mongo

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 const cors = require('cors');
 // Replace <your-connection-string> with your MongoDB Atlas connection string
-const uri = 'mongodb+srv://user1:LqTLU8Tf6w6raxqr@dbcluster.cah1q7r.mongodb.net/sample_mflix?retryWrites=true&w=majority';
+const uri = 'mongodb+srv://<user>:<Password>@<dbclusterurl>/<collectionName>?retryWrites=true&w=majority';
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
